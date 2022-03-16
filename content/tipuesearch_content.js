7ed9434f6e3aecad928c1159d14a44ce40325445
40923142 <40923142@gm.nfu.edu.tw>
2022-03-16 11:47:49 +0800

    one video

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n', 'tags': '', 'url': 'About.html'}, {'title': '每周進度', 'text': '', 'tags': '', 'url': '每周進度.html'}, {'title': 'w3 Film', 'text': 'TECH 4472-Homework 3A-Expressions(第六部 影片) \n 這部影片解釋單位設定、繪畫過程、填料的設定 \n 例:在功能表設定d的尺度，再填料時使用3/4d這種輸入法設定零件的大小。 \n \n \n \n \n   \n \n \n \n \n \n \n \n', 'tags': '', 'url': 'w3 Film.html'}]};
